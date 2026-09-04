815351375fc9dfa0a081cc66eca21ec63d89678d
Gitoldor <rajguruowais@gmail.com>
2026-09-05 01:47:31 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,8 @@
 const mediaData = [
+
   {
     id: "v1",
+    type: "video",
     title: "Building a Fullstack Web App from Scratch",
     url: "https://www.youtube.com/watch?v=nu_pCVPKzTk",
     location: "Sawantwadi, Maharashtra"
@@ -8,6 +10,7 @@ const mediaData = [
 
   {
     id: "v2",
+    type: "video",
     title: "Learn JavaScript in 1 Hour",
     url: "https://www.youtube.com/watch?v=PkZNo7MFNFg",
     location: "Mumbai, Maharashtra"
@@ -15,8 +18,26 @@ const mediaData = [
 
   {
     id: "v3",
+    type: "video",
     title: "HTML Full Course",
     url: "https://www.youtube.com/watch?v=pQN-pnXPaVg",
     location: "Goa, India"
+  },
+
+  {
+    id: "v4",
+    type: "video",
+    title: "CSS Full Course",
+    url: "https://www.youtube.com/watch?v=OXGznpKZ_sA",
+    location: "Pune, Maharashtra"
+  },
+
+  {
+    id: "v5",
+    type: "video",
+    title: "JavaScript DOM Tutorial",
+    url: "https://www.youtube.com/watch?v=5fb2aPlgoys",
+    location: "Nashik, Maharashtra"
   }
+
 ];
