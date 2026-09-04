ff36acd9b117a03376650b54c98bf946715cd63f
Gitoldor <rajguruowais@gmail.com>
2026-09-05 01:39:17 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+const mediaData = [
+  {
+    id: "v1",
+    title: "Building a Fullstack Web App from Scratch",
+    url: "https://www.youtube.com/watch?v=nu_pCVPKzTk",
+    location: "Sawantwadi, Maharashtra"
+  },
+
+  {
+    id: "v2",
+    title: "Learn JavaScript in 1 Hour",
+    url: "https://www.youtube.com/watch?v=PkZNo7MFNFg",
+    location: "Mumbai, Maharashtra"
+  },
+
+  {
+    id: "v3",
+    title: "HTML Full Course",
+    url: "https://www.youtube.com/watch?v=pQN-pnXPaVg",
+    location: "Goa, India"
+  }
+];
